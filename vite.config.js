31a3f8dc80570e5af45be5fac1772a46a78c6db5
Enzo Couto <enzo@gec.inatel.br>
2026-03-16 21:06:19 -0300

    fix: padroniza diretivas do tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/react-plugin'
+
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
